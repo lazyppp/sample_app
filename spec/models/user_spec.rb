913efc156df9d3f7c8d0d959924d15060207ff0a
lazyppp <hino@gakko-net.co.jp>
2014-02-18 01:18:11 +0900

    chp 6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -91,4 +91,12 @@ describe User do
     end
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM"}
+    it "should be save as all lower case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
 end
